816fb3cd6c543af4339ae2d84e121c07ae34e7f7
2730747608@qq.com <2730747608@qq.com>
2021-01-29 08:59:22 +0800

    提交

--- vue_shop/src/components/Login.vue
+++ vue_shop/src/components/Login.vue
@@ -6,19 +6,32 @@
         <img src="../assets/logo.png" alt="" />
       </div>
       <!-- 登录表单区 -->
-      <el-form ref="form" :model="loginForm" label-width="0px" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :rules="loginFormRules"
+        :model="loginForm"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户名 -->
-        <el-form-item>
-          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
-          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -27,12 +40,36 @@
 
 <script>
 export default {
-  data(){
-    return{
-      loginForm:{
-        username:'zs',
-        password:'123'
+  data() {
+    return {
+      loginForm: {
+        username: "龚凯真帅",
+        password: "666666"
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入登录名称", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
+        ]
       }
+    };
+  },
+  methods: {
+    resetLoginForm: function() {
+      // console.log(this);
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        // console.log(valid);
+        if (!valid) return;
+       const {data:res}=await this.$http.post("login", this.loginForm);
+       console.log(res);
+      });
     }
   }
 };
@@ -71,15 +108,15 @@ export default {
     background-color: #eee;
   }
 }
-.login_form{
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
-.btns{
-    display: flex;
-    justify-content: flex-end;
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
