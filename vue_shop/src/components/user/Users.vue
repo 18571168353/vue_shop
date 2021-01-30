cf5331cfae1805419e7d67f99fef6169de02bf1a
2730747608@qq.com <2730747608@qq.com>
2021-01-30 17:32:17 +0800

    删除用户功能完成

--- vue_shop/src/components/user/Users.vue
+++ vue_shop/src/components/user/Users.vue
@@ -61,6 +61,7 @@
               type="danger"
               size="mini"
               icon="el-icon-delete"
+              @click="removeUserById(scope.row.id)"
             ></el-button>
             <!-- 分配角色 -->
             <el-tooltip
@@ -326,6 +327,29 @@ export default {
         // 提示修改成功
         this.$message.success("更新用户信息成功!");
       });
+    },
+    // 根据ID删除用户的信息
+    async removeUserById(id) {
+      // 询问用户是否删除
+      const confirmResult = await this.$confirm(
+        "此操作将永久删除该用户, 是否继续?",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }
+      ).catch(err => err);
+
+      if (confirmResult !== "confirm") {
+        return this.$message.info("已经取消删除");
+      }
+      const { data: res } = await this.$http.delete("users/" + id);
+      if(res.meta.status!=200){
+        return this.$message.error('删除用户失败!')
+      }
+      this.$message.success('删除用户成功!')
+      this.getUserList()
     }
   }
 };
